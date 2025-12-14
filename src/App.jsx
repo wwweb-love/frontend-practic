ea798d21d0223f519529f58eca6cf503ccc469da
Roman <rroman2115@yandex.ru>
2025-12-14 15:57:15 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+
+const Div = styled.div`
+  color: red;
+`
+
+function App() {
+  return (
+    <>
+      <Div>123</Div>
+    </>
+  )
+}
+
+export default App
